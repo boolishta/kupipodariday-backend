efbffeb66a4e37759558c3d4d5ad6c505afcec7b
bator <batr.fly@mail.ru>
2023-12-15 08:42:00 +0100

    import entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { Offer } from './offers/entities/offer.entity';
+import { Wishlist } from './wishlists/entities/wishlist.entity';
 
 @Module({
   imports: [
@@ -12,7 +15,7 @@ import { User } from './users/entities/user.entity';
       username: 'student',
       password: 'student',
       database: 'nest_project',
-      entities: [User],
+      entities: [User, Wish, Offer, Wishlist],
       synchronize: true,
       schema: 'nest_project',
     }),
